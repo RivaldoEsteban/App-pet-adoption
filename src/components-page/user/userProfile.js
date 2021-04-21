a1a2186c76a991ed3fbb2e55f1f9621bbfc00992
Rivaldo Esteban <63760024+RivaldoEsteban@users.noreply.github.com>
2021-04-21 18:25:48 -0500

    modificando el modal user , 100% al alto de imagen que se envia para el perfil

--- src/components-page/user/userProfile.js
+++ src/components-page/user/userProfile.js
@@ -25,64 +25,62 @@ function UserProfile() {
     });
   }
   return (
-    <section>
-      <section className="user-profile">
-        <h2 className="title-page">Perfil</h2>
-        <div className="profile-container">
+    <section className="user-profile">
+      <h2 className="title-page">Perfil</h2>
+      <div className="profile-container">
+        {user.image ? (
+          <img className="image-profile" src={user.image} alt="" />
+        ) : (
+          ""
+        )}
+        <p className="user-name">
+          {user.name} {user.lastName}
+        </p>
+      </div>
+      <form action="" onSubmit={saveData} id="form">
+        <div className="form-container">
+          <label className="label">Nombre</label>
+          <input
+            className="input"
+            type="text"
+            name="user-name"
+            id="input-name"
+          />
+        </div>
+        <div className="form-container">
+          <label className="label">Apellido</label>
+          <input
+            className="input"
+            type="text"
+            name="user-lastname"
+            id="input-lastname"
+          />
+        </div>
+        <div className="form-container">
+          <label className="label">Correo</label>
+          <input
+            className="input"
+            type="email"
+            name="user-email"
+            id="input-email"
+          />
+        </div>
+        <div className="form-container">
+          <label className="label">Foto</label>
+          <input
+            className="input file"
+            type="file"
+            name="user-img"
+            id="photo"
+          />
           {user.image ? (
-            <img className="image-profile" src={user.image} alt="" />
+            <img className="input-file" src={user.image} alt="" />
           ) : (
             ""
           )}
-          <p className="user-name">
-            {user.name} {user.lastName}
-          </p>
         </div>
-        <form action="" onSubmit={saveData} id="form">
-          <div className="form-container">
-            <label className="label">Nombre</label>
-            <input
-              className="input"
-              type="text"
-              name="user-name"
-              id="input-name"
-            />
-          </div>
-          <div className="form-container">
-            <label className="label">Apellido</label>
-            <input
-              className="input"
-              type="text"
-              name="user-lastname"
-              id="input-lastname"
-            />
-          </div>
-          <div className="form-container">
-            <label className="label">Correo</label>
-            <input
-              className="input"
-              type="email"
-              name="user-email"
-              id="input-email"
-            />
-          </div>
-          <div className="form-container">
-            <label className="label">Foto</label>
-            <input
-              className="input file"
-              type="file"
-              name="user-img"
-              id="photo"
-            />
-            {user.image ? (
-              <img className="input-file" src={user.image} alt="" />
-            ) : (
-              ""
-            )}
-          </div>
-          <button className="btn">Guardar</button>
-        </form>
-      </section>
+        <button className="btn">Guardar</button>
+      </form>
     </section>
   );
 }
